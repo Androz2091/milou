acdeb38ab69f74f1dc6603e7478977c9fd54866a
Androz2091 <androz2091@gmail.com>
2021-04-05 11:24:20 +0200

    :tada: First (and probably last) commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Milou
+
+Milou is a simple website that can rewrite your English sentences in a much better way.
+
+## Examples
+
+### Input
+
+```
+Hallo, I want to present Milou, a simple website able to rewrite your English phrases in a many better way
+```
+
+### Output
+
+```
+Hello, I would like to introduce Milou, a simple website that can rewrite your English sentences in a much better way
+```
+
+It's pretty useless and slow but it's fun.
---
+++ index.js
@@ -0,0 +1,33 @@
+const { translate } = require('deepl-scraper');
+const express = require('express');
+const app = express();
+const TextDiff = require('text-diff');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const languageToImprove = ['en', 'en-US'];
+const intermediateLanguage = ['fr', 'fr-FR'];
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        result: false
+    });
+});
+
+app.post('/improve', async (req, res) => {
+    if (!req.body.source) return res.redirect('/');
+    console.log(req.body.source) // ssss
+    const translated = await translate(req.body.source, languageToImprove[0], intermediateLanguage[1]);
+    const toSource = await translate(translated.target.translation, intermediateLanguage[0], languageToImprove[1]);
+    const diff = new TextDiff();
+    const codeDiff = diff.main(req.body.source, toSource.target.translation);
+    res.render('index', {
+        result: true,
+        resultContent: toSource.target.translation,
+        resultContentDiff: diff.prettyHtml(codeDiff)
+    });
+});
+
+app.listen(4000);
